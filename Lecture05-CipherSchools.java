309d1a7652ffae5dc9e54a6ecf0704770fc7f785
Satyam kumar <118036095+Satyam00237@users.noreply.github.com>
2024-06-25 10:53:00 +0530

    Add files via upload

---
+++ Lecture05-CipherSchools.java
@@ -0,0 +1,26 @@
+public class lecture5 {
+    public static void main(String args[]) {
+        float f = (float) 5.5; // by default considers decimal to be double
+        double d = 10.5;
+        float f2 = 5.5f;
+        char ch = 'z';
+        int a=5;
+        int b=9;
+        int c=a+b; //here '+' is an operator rest are operands
+        System.out.println("sum is : "+ z);
+        System.out.println("char is : " + ch);
+        System.out.println("f is : " + f);
+        System.out.println("f2 is : " + f2);
+        System.out.println("d is : " + d);
+
+        //Increment and Decrement Operators
+        //a++; post-increment;
+        System.out.println("Post-Increment of a is a++ : "+ a++);
+
+        //++a; pre-increment;
+        System.out.println("Pre-Increment of a is ++a : "+ ++a);
+        
+        //Similarly for decrement 
+    
+    }
+}
